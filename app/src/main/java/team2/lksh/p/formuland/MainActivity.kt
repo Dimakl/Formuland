a36fd177f7d38289193c8ba765536c6aa2dafbce
TsepaStepan <tsepa.stepan@gmail.com>
2018-07-06 13:51:28 +0300

    Initial commit

---
+++ app/src/main/java/team2/lksh/p/formuland/MainActivity.kt
@@ -0,0 +1,12 @@
+package team2.lksh.p.formuland
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
